4d10946f99e9799ea892b9af218e9537a63b9045
Thanos5544 <thanos554433@gmail.com>
2025-02-07 19:02:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -72,10 +72,10 @@ CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone 
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Username Without @ and Bot Is Admin With Full Right.
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/requestmoviesgroup0')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+QQiRoIMObCcwZThl')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Patrick_Botz')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_Linkl')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'requestmoviesgroup0') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '+QQiRoIMObCcwZThl') # Support Chat Link Without https:// or @
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
