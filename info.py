2371802946b6e05d0e584f0b5c91bd114fc459bc
saroj4400 <sarojkumard0010@gmail.com>
2025-03-23 01:22:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -72,7 +72,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/requestxultron')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Tonystark_botz')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/madyyyyyyyl')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_Linkl')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'MovieTimesXDisc') # Support Chat Link Without https:// or @
 
